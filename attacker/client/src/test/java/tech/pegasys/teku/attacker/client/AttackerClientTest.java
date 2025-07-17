2647f76e2bcbdc3822bde42f120bf855c59f0eba
sager <xueqian1991@gmail.com>
2025-07-17 16:11:15 +0800

    fix spot error

--- attacker/client/src/test/java/tech/pegasys/teku/attacker/client/AttackerClientTest.java
+++ attacker/client/src/test/java/tech/pegasys/teku/attacker/client/AttackerClientTest.java
@@ -1,299 +1,50 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package tech.pegasys.teku.attacker.client;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-import java.util.concurrent.ExecutionException;
-
+import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
 
 public class AttackerClientTest {
 
-    private static final long SLOT = 1234L;
-    private static final String PUBKEY = "0x8f763c496d974ef3e641428bb863a9c7fe85498bcd2f78b26cc1d7ee3a312eb715e9c8736f5b305c68c4d2eb25bd0f78";
-    private static final String BLOCK_DATA = "blockDataBase64";
-    private static final String SIGNED_BLOCK_DATA = "signedBlockDataBase64";
-    private static final String PARENT_ROOT = "0xabcdef1234567890";
-    private static final String ATTEST_DATA = "attestDataBase64";
-    private static final String SIGNED_ATTEST_DATA = "signedAttestDataBase64";
+  private JsonRpcHttpClient rpcClient;
+  private AttackerClient attackerClient;
 
-    private RpcConnection rpcConnection;
-    private AttackerClient attackerClient;
+  @BeforeEach
+  public void setup() throws Exception {
+    rpcClient = mock(JsonRpcHttpClient.class);
+    RpcConnection rpcConnection = new HttpRpcConnection(rpcClient);
+    attackerClient = new AttackerClient(rpcConnection, 0);
+  }
 
-    @BeforeEach
-    public void setup() {
-        rpcConnection = mock(RpcConnection.class);
-        attackerClient = new AttackerClient(rpcConnection, 0);
-    }
+  @Test
+  public void shouldCallBlockGetNewParentRoot() throws Exception {
+    AttackerResponse expectedResponse =
+        new AttackerResponse(AttackerCommand.CMD_CONTINUE, "success");
+    when(rpcClient.invoke(eq("block_getNewParentRoot"), any(), eq(AttackerResponse.class)))
+        .thenReturn(expectedResponse);
 
-    @Test
-    public void shouldCallBlockGetNewParentRoot() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
+    SafeFuture<AttackerResponse> future =
+        attackerClient.blockGetNewParentRoot(1234L, "pubkey", "parentRoot");
+    AttackerResponse response = future.get();
 
-        SafeFuture<AttackerResponse> future = attackerClient.blockGetNewParentRoot(SLOT, PUBKEY, PARENT_ROOT);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("block_getNewParentRoot"),
-                eq(AttackerResponse.class),
-                eq(SLOT),
-                eq(PUBKEY),
-                eq(PARENT_ROOT));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallDelayForReceiveBlock() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.delayForReceiveBlock(SLOT);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("block_delayForReceiveBlock"),
-                eq(AttackerResponse.class),
-                eq(SLOT));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallBlockBeforeBroadcast() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.blockBeforeBroadcast(SLOT);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("block_beforeBroadCast"),
-                eq(AttackerResponse.class),
-                eq(SLOT));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallBlockAfterBroadcast() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.blockAfterBroadcast(SLOT);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("block_afterBroadCast"),
-                eq(AttackerResponse.class),
-                eq(SLOT));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallBlockBeforeSign() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.blockBeforeSign(SLOT, PUBKEY, BLOCK_DATA);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("block_beforeSign"),
-                eq(AttackerResponse.class),
-                eq(SLOT),
-                eq(PUBKEY),
-                eq(BLOCK_DATA));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallBlockAfterSign() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.blockAfterSign(SLOT, PUBKEY, SIGNED_BLOCK_DATA);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("block_afterSign"),
-                eq(AttackerResponse.class),
-                eq(SLOT),
-                eq(PUBKEY),
-                eq(SIGNED_BLOCK_DATA));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallBlockBeforePropose() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.blockBeforePropose(SLOT, PUBKEY, SIGNED_BLOCK_DATA);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("block_beforePropose"),
-                eq(AttackerResponse.class),
-                eq(SLOT),
-                eq(PUBKEY),
-                eq(SIGNED_BLOCK_DATA));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallBlockAfterPropose() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.blockAfterPropose(SLOT, PUBKEY, SIGNED_BLOCK_DATA);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("block_afterPropose"),
-                eq(AttackerResponse.class),
-                eq(SLOT),
-                eq(PUBKEY),
-                eq(SIGNED_BLOCK_DATA));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallAttestBeforeBroadcast() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.attestBeforeBroadcast(SLOT);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("attest_beforeBroadCast"),
-                eq(AttackerResponse.class),
-                eq(SLOT));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallAttestAfterBroadcast() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.attestAfterBroadcast(SLOT);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("attest_afterBroadCast"),
-                eq(AttackerResponse.class),
-                eq(SLOT));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallAttestBeforeSign() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.attestBeforeSign(SLOT, PUBKEY, ATTEST_DATA);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("attest_beforeSign"),
-                eq(AttackerResponse.class),
-                eq(SLOT),
-                eq(PUBKEY),
-                eq(ATTEST_DATA));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallAttestAfterSign() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.attestAfterSign(SLOT, PUBKEY, SIGNED_ATTEST_DATA);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("attest_afterSign"),
-                eq(AttackerResponse.class),
-                eq(SLOT),
-                eq(PUBKEY),
-                eq(SIGNED_ATTEST_DATA));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallAttestBeforePropose() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.attestBeforePropose(SLOT, PUBKEY, SIGNED_ATTEST_DATA);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("attest_beforePropose"),
-                eq(AttackerResponse.class),
-                eq(SLOT),
-                eq(PUBKEY),
-                eq(SIGNED_ATTEST_DATA));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldCallAttestAfterPropose() throws Exception {
-        mockRpcResponse(AttackerCommand.CMD_CONTINUE, "success");
-
-        SafeFuture<AttackerResponse> future = attackerClient.attestAfterPropose(SLOT, PUBKEY, SIGNED_ATTEST_DATA);
-        AttackerResponse response = future.get();
-
-        verify(rpcConnection).call(
-                eq("attest_afterPropose"),
-                eq(AttackerResponse.class),
-                eq(SLOT),
-                eq(PUBKEY),
-                eq(SIGNED_ATTEST_DATA));
-        assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
-        assertThat(response.getResult()).isEqualTo("success");
-    }
-
-    @Test
-    public void shouldHandleDifferentCommandTypes() throws Exception {
-        // Test handling of each command type
-        for (AttackerCommand command : AttackerCommand.values()) {
-            String result = "result for " + command.name();
-            mockRpcResponse(command, result);
-
-            SafeFuture<AttackerResponse> future = attackerClient.blockBeforeSign(SLOT, PUBKEY, BLOCK_DATA);
-            AttackerResponse response = future.get();
-
-            assertThat(response.getCmd()).isEqualTo(command);
-            assertThat(response.getResult()).isEqualTo(result);
-
-            // Test utility methods
-            if (command == AttackerCommand.CMD_CONTINUE) {
-                assertThat(response.shouldContinue()).isTrue();
-            } else {
-                assertThat(response.shouldContinue()).isFalse();
-            }
-
-            if (command == AttackerCommand.CMD_ABORT) {
-                assertThat(response.shouldAbort()).isTrue();
-            } else {
-                assertThat(response.shouldAbort()).isFalse();
-            }
-        }
-    }
-
-    private void mockRpcResponse(AttackerCommand cmd, String result) {
-        AttackerResponse response = new AttackerResponse(cmd, result);
-        SafeFuture<AttackerResponse> future = SafeFuture.completedFuture(response);
-        when(rpcConnection.call(anyString(), eq(AttackerResponse.class), any())).thenReturn(future);
-    }
+    assertThat(response.getCmd()).isEqualTo(AttackerCommand.CMD_CONTINUE);
+    assertThat(response.getResult()).isEqualTo("success");
+  }
 }
--- beacon/validator/src/main/java/tech/pegasys/teku/validator/coordinator/publisher/AbstractBlockPublisher.java
+++ beacon/validator/src/main/java/tech/pegasys/teku/validator/coordinator/publisher/AbstractBlockPublisher.java
@@ -104,11 +104,12 @@ public abstract class AbstractBlockPublisher implements BlockPublisher {
       // when broadcast validation is disabled, we can publish the block (and blob sidecars)
       // immediately and then import
 
-        // todo: luxq add bf inject point, for receiveBlock.
+      // todo: luxq add bf inject point, for receiveBlock.
       importBlobSidecars(blobSidecars.get(), blockPublishingPerformance);
-      SafeFuture<BlockImportAndBroadcastValidationResults> future =  importBlock(block, broadcastValidationLevel, blockPublishingPerformance);
+      SafeFuture<BlockImportAndBroadcastValidationResults> future =
+          importBlock(block, broadcastValidationLevel, blockPublishingPerformance);
 
-        // todo: luxq add bf inject point, for broadcastBlock.
+      // todo: luxq add bf inject point, for broadcastBlock.
       publishBlockAndBlobs(block, blobSidecars, blockPublishingPerformance);
 
       return future;
